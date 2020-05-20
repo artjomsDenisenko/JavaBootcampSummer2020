f3ea78744d3dee07c71908c4e76358157ec9518e
j-sce <j.scerbicka@gmail.com>
2020-05-20 20:26:57 +0300

    extra03

--- src/main/java/jtm/extra03/PracticalNumbers.java
+++ src/main/java/jtm/extra03/PracticalNumbers.java
@@ -1,51 +1,52 @@
 package jtm.extra03;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class PracticalNumbers {
 
-	// TODO Read article https://en.wikipedia.org/wiki/Practical_number
+	// Read article https://en.wikipedia.org/wiki/Practical_number
 	// Implement method, which returns practical numbers in given range
 	// including
 	public String getPracticalNumbers(int from, int to) {
-		ArrayList<Integer> result = new ArrayList<Integer>();
-		for(int num=from; num<=to;num++) {
-			boolean isPractical = false;
-			ArrayList <Integer> factorList = new ArrayList <Integer>();
-
-			for (int i = 1; i < num; i++) {
-				if (num % i == 0) {
-					factorList.add(i);
-				}
+		ArrayList <Integer> practicalNumbers = new ArrayList<Integer>();
+		
+		for(int num = from; num<=to; num++){
+			if(isPractical(num)){
+				practicalNumbers.add(num);
 			}
+		}
+		return practicalNumbers.toString();
+	}
 
-			int[] factors = new int[factorList.size()];
-			for (int i = 0; i < factors.length; i++) {
-				factors[i] = factorList.get(i);
-			}
+	public static boolean isPractical(int num)
+	{
+		ArrayList <Integer> factorList = new ArrayList<Integer>();
 
-			for (int i = num - 1; i > 0; i--) {
-				int temp = i;
-				for (int j = factors.length - 1; j >= 0; j--) {
-					if (temp > 0) {
-						temp -= factors[j];
-					}else if (temp < 0) {
-						temp += factors[j];
-					}else if (temp == 0){
-						break;
-					}
-				}
-				if (temp != 0) {
-					isPractical = false;
-				}else {
-					isPractical = true;
-				}
+		for(int i=1;i<num;i++){
+			if(num%i == 0){
+				factorList.add(i);
 			}
-			if(isPractical=true){
-				result.add(num);}
 		}
 
-		return result.toString();
+		int[] factors = new int[factorList.size()];
+		for(int i =0;i<factors.length;i++) {
+			factors[i] = factorList.get(i).intValue();
+		}
+
+		for(int i = num-1;i>0;i--) {
+			int temp = i;
+			for(int j= factors.length-1;j>=0;j--) {
+				if(temp>0)
+					temp -= factors[j];
+				if(temp <0)
+					temp+= factors[j];
+				if(temp == 0)
+					break;
+			}
+			if(temp!= 0 )
+				return false;
+		}
+		return true;
 	}
+
 }
